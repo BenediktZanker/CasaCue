bf78dd8bb3f5534dd7f74813da32e6293244fdb2
Benedikt <benedikt.zanker@yahoo.de>
2024-10-22 23:26:51 +0200

    Add .NET Backend, test project, and CI configuration

---
+++ CasaCue.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace CasaCue.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int expected = 5;
+            int actual = 2 + 3;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
